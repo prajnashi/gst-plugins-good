b111bee6552ebb629687c64382c9bf0922eaa54a
Edward Hervey <bilboed@bilboed.com>
2009-04-01 17:46:20 +0200

    Wrap id3demux, required for mp3 playback

--- Android.mk
+++ Android.mk
@@ -4,6 +4,7 @@ GST_PLUGINS_GOOD_TOP := $(LOCAL_PATH)
 
 include $(CLEAR_VARS)
 
+include $(GST_PLUGINS_GOOD_TOP)/gst/id3demux/Android.mk
 include $(GST_PLUGINS_GOOD_TOP)/gst/qtdemux/Android.mk
 include $(GST_PLUGINS_GOOD_TOP)/gst/wavparse/Android.mk
 include $(GST_PLUGINS_GOOD_TOP)/gst/videofilter/Android.mk
---
+++ gst/id3demux/Android.mk
@@ -0,0 +1,41 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= 	\
+	gstid3demux.c	\
+	id3tags.c	\
+	id3v2frames.c
+
+LOCAL_SHARED_LIBRARIES :=	\
+	libgsttag-0.10		\
+	libgstreamer-0.10	\
+	libgstbase-0.10		\
+	libglib-2.0		\
+	libgthread-2.0		\
+	libgmodule-2.0		\
+	libgobject-2.0		\
+	libz
+
+LOCAL_MODULE:= libgstid3demux
+
+LOCAL_C_INCLUDES := 				\
+	$(LOCAL_PATH)				\
+	$(GST_PLUGINS_GOOD_TOP)			\
+	external/gst-plugins-base/gst-libs 	\
+	external/gstreamer		 	\
+	external/gstreamer/android 		\
+	external/gstreamer/libs			\
+	external/gstreamer/gst			\
+	external/gstreamer/gst/android		\
+	external/glib			  	\
+	external/glib/android	  		\
+	external/glib/glib		 	\
+	external/glib/gmodule	  		\
+	external/glib/gobject	  		\
+	external/glib/gthread
+
+LOCAL_CFLAGS := \
+	-DHAVE_CONFIG_H			
+
+include $(BUILD_PLUGIN_LIBRARY)
